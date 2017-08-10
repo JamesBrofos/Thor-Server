1a1472739ef186ace07723e9286a48554590ae43
Daniel Chudnov <dlchudnov@mitre.org>
2017-08-10 18:53:40 -0400

    added experiment admin view with publish/unpublish and delete, added migrations, depends on #5, closes #3

--- website/views/index.py
+++ website/views/index.py
@@ -1,18 +1,23 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
 
+from ..models import Experiment
+
 
 index = Blueprint("index", __name__)
 # Define the number of experiments that will appear on a page.
 EXPERIMENTS_PER_PAGE = 10
 
+
 @index.route("/")
 @index.route("/<int:page>")
 def page(page=1):
     try:
-        experiments = current_user.experiments.order_by("date desc").paginate(
-            page, EXPERIMENTS_PER_PAGE, False
-        )
+        experiments = current_user.experiments.order_by(
+            Experiment.date.desc()
+            ).paginate(
+                page, EXPERIMENTS_PER_PAGE, False
+                )
     except AttributeError:
         experiments = None
 
