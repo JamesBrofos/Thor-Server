24295ba23b82fdc684601655832e254a3d7e378b
Daniel Chudnov <dlchudnov@mitre.org>
2017-08-14 14:01:22 -0400

    added basic testing framework and tests, support libs in requirements, "make test" in makefile

---
+++ website/config.py
@@ -0,0 +1,11 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://thor_server:thor@localhost:5432/thor"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = "50601550186443463175"
+
+MAIL_PREFIX = "[Thor]"
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = "this.is.a.gmail.account@gmail.com"
+MAIL_PASSWORD = "this.is.a.password"
